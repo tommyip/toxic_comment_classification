4054d2a1f267b38b050c4e168654050d8baf1610
Thomas Ip <thomas@ipthomas.com>
2020-11-19 14:34:19 +0800

    Manually rebalance dataset

--- helper.py
+++ helper.py
@@ -4,11 +4,11 @@ from sklearn.model_selection import train_test_split
 from config import TRAIN_SIZE, VALIDATION_SIZE, TEST_SIZE
 
 
-def train_validation_test_data():
+def load_data():
     """
     @returns x_train, x_validation, x_test, y_train, y_validation, y_test,
     """
-    df = pd.read_csv('data/train.csv', usecols=range(1, 8))
+    df = pd.read_pickle('train.pickle')
     x_train, x_alt, y_train, y_alt = train_test_split(
         df['comment_text'], df.drop(columns='comment_text'),
         train_size=TRAIN_SIZE,
@@ -18,6 +18,6 @@ def train_validation_test_data():
         x_alt, y_alt,
         train_size=VALIDATION_SIZE,
         test_size=TEST_SIZE,
-        random_state=1337)
+        random_state=42)
 
     return x_train, x_validation, x_test, y_train, y_validation, y_test
